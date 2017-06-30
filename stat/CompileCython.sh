5ed24a80102d84cc81a4985f09578d7b2a9e6f17
Boß <pdrewe@ohler19.mdc-berlin.net>
2017-06-30 11:37:52 +0200

    updated script to compile the viterby algorithm

--- stat/CompileCython.sh
+++ stat/CompileCython.sh
@@ -4,4 +4,4 @@ set -e
 
 cython viterbi.pyx
 
-gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/home/pdrewe/software/anaconda2/include/python2.7 -o viterbi.so viterbi.c
+gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o viterbi.so viterbi.c
