0b313260961f636aac63ea87d0f64d91e11005e7
Boß <pdrewe@ohler19.mdc-berlin.net>
2017-07-03 16:09:45 +0200

    Added script to create the annotation database

---
+++ data_parsing/CreateGeneAnnotDB.py
@@ -0,0 +1,19 @@
+import gffutils
+import sys
+
+def CreateDB(FileNameGFF, FileNameDB):
+    '''
+    This function creates a GFF database
+    '''
+
+    db = gffutils.create_db(FileNameGFF, dbfn=FileNameDB, force=True, keep_order=True, merge_strategy='merge', sort_attribute_values=True)
+    return
+
+if __name__ == "__main__":
+    FileNameGFF = sys.argv[1]
+    
+    FileNameDB = sys.argv[2]
+    print 'Creatin annotation database'
+    CreateDB(FileNameGFF, FileNameDB)
+
+
