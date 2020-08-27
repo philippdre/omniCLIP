f578a22a62a282088d28565e7087da37311830c8
Joel Simoneau <Joel.Simoneau@mdc-berlin.de>
2020-08-27 11:08:09 -0700

    setup for pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ from setuptools.extension import Extension
 
 import numpy as np
 
-long_description = "This is a long description"
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='omniCLIP_test',
-    version="0.1a",
+    version="0.1a7",
     description='Test of the omniCLIP project',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Philipp Boss',
     author_email='joel.simoneau@mdc-berlin.de',
     url='https://github.com/simojoe/omniCLIP',
@@ -23,14 +28,19 @@ setup(
         include_dirs=[np.get_include()],
     )],
     zip_safe=False,
-    entry_points ={
+    entry_points={
         'console_scripts': [
             'omniCLIP = omniCLIP.omniCLIP:main'
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     setup_requires=['numpy>=1.18', 'cython>=0.24.1'],
     install_requires=[
-        'numpy>=1.18', 'nose>=0.11', 'cython>=0.24.1',
+        'numpy>=1.18', 'nose>=0.11', 'cython>=0.24.1', 'h5py>=2.10.0',
         'statsmodels>=0.11.0', 'scipy>=1.4.1', 'scikit-learn>=0.22.1',
-        'pysam>=0.15.3', 'pandas>=1.0.2', 'intervaltree>=3.0.2', 'h5py>=2.10.0', 'gffutils>=0.10.1'],
+        'pysam>=0.15.3', 'pandas>=1.0.2', 'intervaltree>=3.0.2',
+        'gffutils>=0.10.1', 'biopython>=1.76'],
 )
